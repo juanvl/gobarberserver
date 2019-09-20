c4a6686a583f0b24017c8cbcacd6d89f94437f54
Juan <jnvleite@gmail.com>
2019-09-20 12:11:03 -0300

    user api adjustments

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -37,10 +37,10 @@ class SessionController {
       return res.status(401).json({ error: 'Senha incorreta!' });
     }
 
-    const { id, name, avatar } = user;
+    const { id, name, avatar, provider } = user;
 
     return res.json({
-      user: { id, name, email, avatar },
+      user: { id, name, email, avatar, provider },
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
       }),
